4b5167b321f6a04dd5397325c802b98f544fb351
“Ore_Steph_is668” <ssangah1@umbc.edu>
2020-04-27 00:06:08 +0000

    initialize db

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,25 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="teamstore20",
+    password="gradebook123",
+    hostname="teamstore20.mysql.pythonanywhere-services.com",
+    databasename="teamstore20$gradebookdb",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+
 
