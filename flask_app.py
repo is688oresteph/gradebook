3611a314f02700a8cfd9b4d621af0e44a59e3751
“Ore_Steph_is668” <ssangah1@umbc.edu>
2020-04-28 00:55:28 +0000

    created db

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -19,8 +20,43 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+
+class Student(db.Model):
+
+    __tablename__ = "student"
+
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(128))
+    LastName = db.Column(db.String(128))
+    Major = db.Column(db.String(128))
+    Email = db.Column(db.String(128))
+    MyGrade = db.relationship('Grade', backref='sname')
+
+class Assignment(db.Model):
+
+    __tablename__ = "assignment"
+
+    id = db.Column(db.Integer, primary_key=True)
+    AssignmentName = db.Column(db.String(128))
+    PointsTotal = db.Column(db.Integer)
+    AGrade = db.relationship('Grade', backref='assignment')
+
+
+class Grade(db.Model):
+
+    __tablename__ = "grade"
+
+    id = db.Column(db.Integer, primary_key=True)
+    Assignment_ID = db.Column(db.Integer, db.ForeignKey('assignment.id'))
+    Student_ID = db.Column(db.Integer, db.ForeignKey('student.id'))
+    Grade = db.Column(db.Float)
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
 
 
